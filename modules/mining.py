b786bda0695a24004dd28166d902faaf4f247c82
Tobias <toby.janca@seznam.cz>
2024-07-31 15:17:28 +0200

    Much better vein detection

--- modules/mining.py
+++ modules/mining.py
@@ -3,22 +3,39 @@ from typing import Sequence
 import mss
 import time
 import cv2
+import pydirectinput
+
 from game_settings import VALUES
-from modules.funcs import take_screenshot, min_max, click, gather_items, reset_camera_to_default, random_movement
+from modules.funcs import take_screenshot, min_max, click, gather_items, reset_camera_to_default, random_movement, \
+    min_max_multiple, distance_from_center, measure_time
 
 
 class MiningBot:
     def __init__(self):
-        self.VEIN_PICTURE = cv2.imread('screenshots/vein_picture.png')
-        self.VEIN_PICTURE2 = cv2.imread('screenshots/vein_picture2.png')
-        self.MINING_SUCCESS = cv2.imread("screenshots/mining_success.png")
-        self.MINING_FAILED = cv2.imread("screenshots/mining_failed.png")
+        self.VEIN_PICTURE = cv2.imread('screenshots/vein.png')
+        self.NEEDLES = [self.VEIN_PICTURE]
+
+        self.MINING_SUCCESS = cv2.imread('screenshots/mining_success.png')
+        self.MINING_FAILED = cv2.imread('screenshots/mining_failed.png')
         self.NOTHING_TO_MINE = cv2.imread('screenshots/nothing_to_mine.png')
         self.logger = logging.getLogger(__name__)
         self.failed_previously = False
         self.stopped_previously = False
+        self.sct = None
 
-    def check_mining_state(self, sct) -> bool:
+    @property
+    def monitor_width(self) -> int:
+        if self.sct is not None:
+            return self.sct.monitors[1]["width"]
+        return 0
+
+    @property
+    def monitor_height(self) -> int:
+        if self.sct is not None:
+            return self.sct.monitors[1]["height"]
+        return 0
+
+    def check_mining_state(self) -> bool:
         time_start = time.time()
 
         while True:
@@ -26,19 +43,24 @@ class MiningBot:
                 self.logger.info("Mining stopped after waiting time")
                 return True
 
-            screenshot = take_screenshot(sct, 777, 970, 260, 75)
+            screenshot = take_screenshot(self.sct, 777, 970, 260, 75)
 
             if not self.failed_previously:
                 mining_failed = min_max(screenshot, [self.MINING_FAILED], threshold=0.12)
                 if mining_failed != -1:
                     self.logger.info("Mining failed, trying to mine again")
+                    x, y = pydirectinput.position()
+                    # Move a mouse a little to fix a click
+                    pydirectinput.moveTo(x + 2, y + 2)
                     self.failed_previously = True
+
                     return False
 
             if not self.stopped_previously:
                 mining_stopped = min_max(screenshot, [self.NOTHING_TO_MINE], threshold=0.12)
                 if mining_stopped != -1:
                     self.logger.info("Mine deposit disappeared, trying to find a new vein")
+                    self.stopped_previously = True
                     return False
 
             if time.time() - time_start > 4:
@@ -49,26 +71,37 @@ class MiningBot:
 
             time.sleep(0.5)
 
-    def find_vein(self, sct) -> Sequence[int] | int:
-        screenshot = take_screenshot(sct)
-        return min_max(screenshot, [self.VEIN_PICTURE, self.VEIN_PICTURE2], threshold=0.15)
+    def find_vein(self) -> Sequence[int] | int:
+        """
+        Tries to find a vein from a screenshot
+        :return: (x, y) if found, -1 if not
+        """
+        screenshot = take_screenshot(self.sct)
+        matches = min_max_multiple(screenshot, self.NEEDLES, threshold=0.2)
 
-    def mine_vein(self, sct) -> bool:
-        top_left = self.find_vein(sct)
+        if matches == -1:
+            return -1
+
+        center_x = self.monitor_width // 2
+        center_y = self.monitor_height // 2
+
+        closest_match = min(matches, key=lambda match: distance_from_center(match, center_x, center_y))
+
+        return closest_match[0], closest_match[1]
+
+    def mine_vein(self):
+        top_left = self.find_vein()
         if top_left == -1:
-            return False
+            random_movement(0.3, 1)
+            return
 
         if click(top_left, 50, 20) is False:
-            return False
+            return
 
-        if self.check_mining_state(sct):
-            time.sleep(0.2)
+        if self.check_mining_state():
             gather_items()
             self.failed_previously = False
             self.stopped_previously = False
-            return True
-
-        return False
 
     def start_mining(self) -> None:
         self.logger.info("STARTING and WAITING -> MINING")
@@ -77,6 +110,6 @@ class MiningBot:
 
         reset_camera_to_default()
         with mss.mss() as sct:
+            self.sct = sct
             while True:
-                while self.mine_vein(sct) is False:
-                    random_movement(0.3, 1)
+                self.mine_vein()
